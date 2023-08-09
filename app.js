8cc4832e8bcdc5f24c79f247733fdae8cb9e3a7d
LeoosMG <leoos.m.e.2019@gmail.com>
2023-08-08 21:17:42 -0300

    Resubiendo Mercado liebre

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/home', (request, response) => {
+    response.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+app.get('/register', (request, response) => {
+    response.sendFile(path.join(__dirname, "./views/register.html"))
+})
+
+app.get('/login', (request, response) => {
+    response.sendFile(path.join(__dirname, "./views/login.html"))
+})
+
+
+const port = process.env.port || 8080;
+app.listen(port, () => {
+    console.log(`[server]: running at port ${port} (http://localhost:${port}/).`);
+})
